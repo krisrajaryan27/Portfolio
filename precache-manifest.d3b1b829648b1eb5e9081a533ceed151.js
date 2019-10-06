d97a301ac975f7781a25fcee9fe448501bf3eb0f
Krishna Verma <krisv.nit16@gmail.com>
2019-10-06 12:55:35 +0530

    Updates

---
+++ precache-manifest.d3b1b829648b1eb5e9081a533ceed151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c99b99038aa5c4daca184e4d900d7c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8101b85802f9d5106b1c",
+    "url": "/Portfolio/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "5c0d8fa494367a9d9ae2",
+    "url": "/Portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8101b85802f9d5106b1c",
+    "url": "/Portfolio/static/js/2.5fce55ae.chunk.js"
+  },
+  {
+    "revision": "5c0d8fa494367a9d9ae2",
+    "url": "/Portfolio/static/js/main.1caf8561.chunk.js"
+  },
+  {
+    "revision": "1c28bae7fae808197899",
+    "url": "/Portfolio/static/js/runtime-main.8f04a0c0.js"
+  }
+]);
